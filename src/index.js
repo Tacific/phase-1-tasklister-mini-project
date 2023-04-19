1c58dc5eb0bad07fa6dbbcd469c305074f1b005c
Chantha Voeun <cvoeun45@gmail.com>
2023-04-18 22:10:46 -0400

    Done NC

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault()
+  buildToDo(e.target.pink.value)
+
+  })
+  
 });
+
+function buildToDo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = ' Done '
+  p.textContent = ` ${ todo } `
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e){
+e.traget.parentNode.remove()
+}
+
+
